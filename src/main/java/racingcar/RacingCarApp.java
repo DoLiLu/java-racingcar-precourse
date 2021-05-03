53eb77d779e95813a76ec9c71ccbf39fe6011b11
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-05-03 23:27:03 +0900

    feat: 자동차 이름 입력 기능 추가
    자동차 이름 입력 기능 추가 및 사용할 컨트롤러 생성

---
+++ src/main/java/racingcar/RacingCarApp.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import racingcar.controller.RacingCarController;
+
+public class RacingCarApp {
+    public static void main(String[] args) {
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.start();
+    }
+}
---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,9 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingCarController {
+    public void start() {
+        String carNames = InputView.carsNameInput();
+    }
+}
